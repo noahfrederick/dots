9a5a24a8692462e771f4d558f061bb026fff713d
Noah Frederick <noah@noahfrederick.com>
2013-06-15 20:16:57 -0400

    Fix redirection in tmux.fish

--- fish/functions/tmux.fish
+++ fish/functions/tmux.fish
@@ -2,6 +2,7 @@ function tmux -d "Attach to existing tmux session if possible"
 	if test (count $argv) -gt 0
 		command tmux $argv
 	else
-		command tmux attach -d &>/dev/null; or command tmux
+		command tmux attach-session -d >/dev/null ^&1
+		or command tmux
 	end
 end
