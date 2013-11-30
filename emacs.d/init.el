e943308a2d9d06c88b021f1db1ccc323d6f5fa85
Noah Frederick <noah@noahfrederick.com>
2013-11-30 14:59:21 -0500

    [Emacs] Add 3rd configuration attempt

---
+++ emacs.d/init.el
@@ -0,0 +1,24 @@
+;;; init.el --- Bootstrap Emacs configuration
+
+;;; Commentary:
+
+;; This file loads Org-mode and then loads the rest of our Emacs
+;; initialization from Emacs Lisp embedded in literate Org-mode files.
+
+;;; Code:
+
+;; Cask manages our package dependencies
+(require 'cask "~/.cask/cask.el")
+(cask-initialize)
+
+;; Pallet allows us to use Cask in tandem with package.el
+(require 'pallet)
+
+;; Load up Org-mode and Org-babel
+(require 'org-install)
+(require 'ob-tangle)
+
+;; Load our main configuration file
+(org-babel-load-file (expand-file-name "emacs.org" user-emacs-directory))
+
+;;; init.el ends here
