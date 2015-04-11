b9675516a98ad6389d6cffc33441163c7d33faab
Noah Frederick <noah@noahfrederick.com>
2015-04-11 10:34:41 -0400

    [Fish] Git prompt fixes

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,18 +1,18 @@
 set -g __fish_git_prompt_show_informative_status 1
 set -g __fish_git_prompt_showuntrackedfiles ""
 
-set -g __fish_git_prompt_color_branch magenta bold
+set -g __fish_git_prompt_color_branch magenta --bold
 set -g __fish_git_prompt_showupstream "informative"
 set -g __fish_git_prompt_char_upstream_ahead "↑"
 set -g __fish_git_prompt_char_upstream_behind "↓"
-set -g __fish_git_prompt_char_upstream_prefix " "
+set -g __fish_git_prompt_char_upstream_prefix ""
 
 set -g __fish_git_prompt_char_stagedstate "+"
 set -g __fish_git_prompt_char_dirtystate "●"
 set -g __fish_git_prompt_char_untrackedfiles "…"
 set -g __fish_git_prompt_char_conflictedstate "x"
 set -g __fish_git_prompt_char_cleanstate ""
-set -g __fish_git_prompt_char_stateseparator " "
+set -g __fish_git_prompt_char_stateseparator ""
 
 set -g __fish_git_prompt_color_dirtystate blue
 set -g __fish_git_prompt_color_stagedstate green
