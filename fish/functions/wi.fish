fb23423f05b426c3f201fb738dcb82391831d7bf
Noah Frederick <noah@noahfrederick.com>
2015-06-18 12:37:27 -0400

    [Fish] Fix Words functions

--- fish/functions/wi.fish
+++ fish/functions/wi.fish
@@ -1,7 +1,10 @@
 function wi -d "Interactive wrapper for Whitaker's Words"
-	echo -n "> "
-	while read line
-		ww $line
-		echo -n "> "
+	while read --prompt=_words_prompt query
+		set_color normal
+		ww $query
 	end
+end
+
+function _words_prompt
+	set_color blue --bold; echo -n "> "
 end
