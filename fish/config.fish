0b635c65bb70a437ca7da429e43ee116eff1d5b1
Noah Frederick <noah@noahfrederick.com>
2016-03-04 17:17:37 -0500

    [Fish] Only define abbrevs for interactive shells

--- fish/config.fish
+++ fish/config.fish
@@ -54,56 +54,58 @@ set -gx FZF_DEFAULT_OPTS "--inline-info"
 #
 # ABBREVIATIONS
 #
-abbr --add art php artisan
-abbr --add br  brew
-abbr --add bru brew upgrade
-abbr --add c   composer
-abbr --add ci  composer install
-abbr --add cu  composer update
-abbr --add cr  composer require
-abbr --add cgi composer global install
-abbr --add cgu composer global update
-abbr --add cgr composer global require
-abbr --add cda composer dump-autoload
-abbr --add ch  composer show
-abbr --add cm  composer remove
-abbr --add cs  composer search
-abbr --add cv  composer validate
-abbr --add e   emacsclient
-abbr --add ee  emacsclient --eval
-abbr --add en  emacsclient --no-wait
-abbr --add et  emacsclient --tty
-abbr --add g   git
-abbr --add ga  git add
-abbr --add gb  git branch
-abbr --add gc  git commit
-abbr --add gca git commit --all
-abbr --add gcl git clone
-abbr --add gco git checkout
-abbr --add gd  git diff
-abbr --add gdc git diff --cached
-abbr --add gf  git fetch
-abbr --add gg  git graph
-abbr --add gi  git init
-abbr --add gl  git log
-abbr --add gp  git push
-abbr --add gpu git pull
-abbr --add gs  git status
-abbr --add gst git stash
-abbr --add gsp git stash pop
-abbr --add lba ledger balance
-abbr --add leq ledger equity
-abbr --add lst ledger stats
-abbr --add lre ledger register
-abbr --add lxa ledger xact
-abbr --add v   nvim
-abbr --add vs  nvim -S
-abbr --add vn  nvim -u NONE
-abbr --add vd  nvim --noplugin -u minimal.vim
-abbr --add wk  webkit2png --fullsize --timestamp
-abbr --add ww  words
-abbr --add we  words --english
-abbr --add wi  words --interactive
+if status --is-interactive
+	abbr --add art php artisan
+	abbr --add br  brew
+	abbr --add bru brew upgrade
+	abbr --add c   composer
+	abbr --add ci  composer install
+	abbr --add cu  composer update
+	abbr --add cr  composer require
+	abbr --add cgi composer global install
+	abbr --add cgu composer global update
+	abbr --add cgr composer global require
+	abbr --add cda composer dump-autoload
+	abbr --add ch  composer show
+	abbr --add cm  composer remove
+	abbr --add cs  composer search
+	abbr --add cv  composer validate
+	abbr --add e   emacsclient
+	abbr --add ee  emacsclient --eval
+	abbr --add en  emacsclient --no-wait
+	abbr --add et  emacsclient --tty
+	abbr --add g   git
+	abbr --add ga  git add
+	abbr --add gb  git branch
+	abbr --add gc  git commit
+	abbr --add gca git commit --all
+	abbr --add gcl git clone
+	abbr --add gco git checkout
+	abbr --add gd  git diff
+	abbr --add gdc git diff --cached
+	abbr --add gf  git fetch
+	abbr --add gg  git graph
+	abbr --add gi  git init
+	abbr --add gl  git log
+	abbr --add gp  git push
+	abbr --add gpu git pull
+	abbr --add gs  git status
+	abbr --add gst git stash
+	abbr --add gsp git stash pop
+	abbr --add lba ledger balance
+	abbr --add leq ledger equity
+	abbr --add lst ledger stats
+	abbr --add lre ledger register
+	abbr --add lxa ledger xact
+	abbr --add v   nvim
+	abbr --add vs  nvim -S
+	abbr --add vn  nvim -u NONE
+	abbr --add vd  nvim --noplugin -u minimal.vim
+	abbr --add wk  webkit2png --fullsize --timestamp
+	abbr --add ww  words
+	abbr --add we  words --english
+	abbr --add wi  words --interactive
+end
 
 # Make fish work with boxen
 set -l FISH_BOXEN ~/.config/fish/boxen.fish
