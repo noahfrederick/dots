ca7a8f609d1f9a4c060074a17b1e5cee4a0b82b2
Noah Frederick <noah@noahfrederick.com>
2016-11-28 15:33:39 -0500

    [Fish] Work around OpenSSL breaking Homebrew

--- fish/functions/brew.fish
+++ fish/functions/brew.fish
@@ -1,4 +1,7 @@
 function brew --description 'Default arguments for brew'
+	set -l OLD_SSL_CERT_FILE $SSL_CERT_FILE
+	set -e SSL_CERT_FILE
+
 	if test (count $argv) -gt 0
 		command brew $argv
 	else
@@ -7,4 +10,6 @@ function brew --description 'Default arguments for brew'
 			(set_color --bold white) ' Outdated Formulae' (set_color normal)
 		and command brew outdated
 	end
+
+	set -gx SSL_CERT_FILE $OLD_SSL_CERT_FILE
 end
