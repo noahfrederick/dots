8f5b836bcc8ca6553aedb3f2048f6b6b86376d8d
Noah Frederick <noah@noahfrederick.com>
2017-05-09 15:37:39 -0400

    [Fish] Fix arguments to fzf in mux

--- fish/functions/mux.fish
+++ fish/functions/mux.fish
@@ -2,9 +2,6 @@ function mux -w tmuxinator -d "Enhanced tmuxinator with fuzzy completion menu"
 	if test (count $argv) -gt 0
 		tmuxinator $argv
 	else
-		# This would be cleaner with command substitution, but interactive
-		# commands get backgrounded. See discussion of this bug:
-		# https://github.com/fish-shell/fish-shell/issues/1362
-		tmuxinator completions start | fzf --select-1 --exit-0 | xargs tmuxinator start
+		tmuxinator completions start | fzf | xargs tmuxinator start
 	end
 end
