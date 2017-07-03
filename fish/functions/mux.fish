5b5e31e95fe5d2f60db45b52ea977b27f9a2a4d0
Noah Frederick <code@noahfrederick.com>
2017-07-03 08:35:59 -0400

    [Tmux] Use fzf-mux for session switching

--- fish/functions/mux.fish
+++ fish/functions/mux.fish
@@ -2,6 +2,6 @@ function mux -w tmuxinator -d "Enhanced tmuxinator with fuzzy completion menu"
 	if test (count $argv) -gt 0
 		tmuxinator $argv
 	else
-		tmuxinator completions start | fzf | xargs tmuxinator start
+		tmuxinator completions start | fzf-tmux -u 30% --reverse | xargs tmuxinator start
 	end
 end
