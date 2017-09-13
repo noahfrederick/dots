648d338a7a6dd406f0875773309935b9b772c4b4
Noah Frederick <code@noahfrederick.com>
2017-09-13 16:47:52 -0400

    [Planck] Convert Makefile to rules.mk
    Per upcoming change in QMK requiring compilation from the firmware's
    root Makefile.

---
+++ planck/rules.mk
@@ -0,0 +1,3 @@
+COMMAND_ENABLE = no   # Disable shift combination, which conflicts with shift-parens
+NKRO_ENABLE    = yes  # N-key rollover required for use as a steno board
+AUDIO_ENABLE   = yes  # Audio output on port C6
