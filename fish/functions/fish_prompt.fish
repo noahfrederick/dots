a9b0f942b315f4f39a2243ca3d397f223ada8ae8
Noah Frederick <code@noahfrederick.com>
2018-01-25 19:29:37 -0500

    [Fish] Add niceties to prompts
    - Indicate presence of background jobs
    - Indicate shell level
    - Less verbose git status

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,5 +1,17 @@
 function fish_prompt --description 'Write out the prompt'
-	set_color brgrey --bold
-	echo -n '> '
+	if jobs > /dev/null
+		set_color green --bold
+	else
+		set_color brgrey --bold
+	end
+
+	set -l level $SHLVL
+
+	if test -n "$TMUX"
+		set level (math $SHLVL - 2) # fish -> tmux -> fish
+	end
+
+	string repeat --no-newline --count $level '> '
+
 	set_color normal
 end
--- fish/functions/fish_right_prompt.fish
+++ fish/functions/fish_right_prompt.fish
@@ -1,5 +1,5 @@
 set -g __fish_git_prompt_show_informative_status 1
-set -g __fish_git_prompt_showuntrackedfiles ""
+set -g __fish_git_prompt_showuntrackedfiles 1
 
 set -g __fish_git_prompt_color_branch magenta --bold
 set -g __fish_git_prompt_showupstream "informative"
@@ -7,12 +7,18 @@ set -g __fish_git_prompt_char_upstream_ahead "↑"
 set -g __fish_git_prompt_char_upstream_behind "↓"
 set -g __fish_git_prompt_char_upstream_prefix " "
 
-set -g __fish_git_prompt_char_stagedstate " +"
-set -g __fish_git_prompt_char_dirtystate " -"
-set -g __fish_git_prompt_char_untrackedfiles "/"
-set -g __fish_git_prompt_char_conflictedstate " x"
+set -g __fish_git_prompt_char_stagedstate "●"
+set -g __fish_git_prompt_char_dirtystate "●"
+set -g __fish_git_prompt_char_untrackedfiles "●"
+set -g __fish_git_prompt_char_conflictedstate "●"
 set -g __fish_git_prompt_char_cleanstate ""
-set -g __fish_git_prompt_char_stateseparator ""
+set -g __fish_git_prompt_char_stateseparator " "
+
+# Hide file counts
+set -g __fish_git_prompt_hide_stagedstate 1
+set -g __fish_git_prompt_hide_invalidstate 1
+set -g __fish_git_prompt_hide_dirtystate 1
+set -g __fish_git_prompt_hide_untrackedfiles 1
 
 set -g __fish_git_prompt_color_dirtystate red
 set -g __fish_git_prompt_color_stagedstate green
