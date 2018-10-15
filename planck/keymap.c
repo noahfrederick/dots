47462c4692bb2a5c71163bf74cedb082a5183b10
Noah Frederick <code@noahfrederick.com>
2018-10-14 21:41:13 -0400

    [Planck] Make alt-tab accessible from steno layer

--- planck/keymap.c
+++ planck/keymap.c
@@ -185,20 +185,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   /* Stenography layer
    *                ┌─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┐
-   *                │  #  │  #  │  #  │  #  │  #  │  #  │  #  │  #  │  #  │  #  │  #  │  #  │
+   *                │  ⇥  │  #  │  #  │  #  │  #  │  #  │  #  │  #  │  #  │  #  │  #  │  #  │
    *                ├─────┼─────┼─────┼─────┼─────┼─────┴─────┼─────┼─────┼─────┼─────┼─────┤
    *                │Res 1│     │  T  │  P  │  H  │           │  F  │  P  │  L  │  T  │  D  │
    *                ├─────┤  S  ├─────┼─────┼─────┤     *     ├─────┼─────┼─────┼─────┼─────┤
    *                │Res 2│     │  K  │  W  │  R  │           │  R  │  B  │  G  │  S  │  Z  │
    *                ├─────┼─────┼─────┼─────┼─────┼─────┬─────┼─────┼─────┼─────┼─────┼─────┤
-   *                │Exit │     │     │     │  A  │  O  │  E  │  U  │     │     │ Pwr │ FN  │
+   *                │Exit │     │  ⌥  │  ⌘  │  A  │  O  │  E  │  U  │  ⌘  │  ⌥  │ Pwr │ FN  │
    *                └─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┘
    */
   [STENO_LAYER] = LAYOUT_planck_grid(
-    STN_N1,   STN_N2,  STN_N3,  STN_N4,  STN_N5, STN_N6,  STN_N7,  STN_N8, STN_N9,  STN_NA,  STN_NB,  STN_NC,
+    KC_TAB,   STN_N2,  STN_N3,  STN_N4,  STN_N5, STN_N6,  STN_N7,  STN_N8, STN_N9,  STN_NA,  STN_NB,  STN_NC,
     STN_RES1, STN_S1,  STN_TL,  STN_PL,  STN_HL, STN_ST1, STN_ST3, STN_FR, STN_PR,  STN_LR,  STN_TR,  STN_DR,
     STN_RES2, STN_S2,  STN_KL,  STN_WL,  STN_RL, STN_ST2, STN_ST4, STN_RR, STN_BR,  STN_GR,  STN_SR,  STN_ZR,
-    STN_EXIT, XXXXXXX, XXXXXXX, XXXXXXX, STN_A,  STN_O,   STN_E,   STN_U,  XXXXXXX, XXXXXXX, STN_PWR, STN_FN
+    STN_EXIT, XXXXXXX, KC_LALT, KC_LGUI, STN_A,  STN_O,   STN_E,   STN_U,  KC_RGUI, KC_RALT, STN_PWR, STN_FN
   ),
 
   /* Keyboard settings layer
