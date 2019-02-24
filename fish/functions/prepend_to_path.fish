a4adb1544f2f986d1f28bf36343cac38ad117e2a
Noah Frederick <code@noahfrederick.com>
2019-02-24 11:38:38 -0500

    [Fish] Set fish_user_paths instead of PATH
    This partially works around
    <https://github.com/fish-shell/fish-shell/issues/5456>.

--- fish/functions/prepend_to_path.fish
+++ fish/functions/prepend_to_path.fish
@@ -1,7 +1,7 @@
 function prepend_to_path -d "Prepend the given dir to PATH if it exists and is not already listed"
 	if test -d "$argv[1]"
-		if not contains "$argv[1]" $PATH
-			set -x PATH "$argv[1]" $PATH
+		if not contains "$argv[1]" $fish_user_paths
+			set --global --prepend fish_user_paths "$argv[1]"
 		end
 	end
 end
