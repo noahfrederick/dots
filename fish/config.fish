5348761af578be509b68be5398ff00ba14933609
Noah Frederick <code@noahfrederick.com>
2020-02-22 22:35:43 -0500

    [Fish] Do not set yarn path
    `yarn global bin` evaluates to /usr/local/bin, which messes up the order
    of paths in `fish_user_paths` (a.k.a. PATH) somehow. I don't understand
    the cause, exactly, but since /usr/local/bin is already present in
    /etc/paths.d/, this line is not needed.

--- fish/config.fish
+++ fish/config.fish
@@ -112,7 +112,6 @@ status is-login; or exit
 # PATH
 #
 prepend_to_path $HOME/.rbenv/bin # For non-brew-installed rbenv
-type -fq yarn;     and prepend_to_path (yarn global bin)
 type -fq composer; and prepend_to_path (composer config --global home)/(composer config --global bin-dir)
 type -fq rbenv;    and source (rbenv init - | psub)
 prepend_to_path $HOME/.cargo/bin
